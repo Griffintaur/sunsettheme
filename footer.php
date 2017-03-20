40bfc76abdbcffe6f76d129f9ff228dcb0e326a0
Ankit Singh <ankitsingh135@gmail.com>
2017-03-20 23:50:32 +0530

    header

--- footer.php
+++ footer.php
@@ -1,1 +1,10 @@
+<?php 
+	/*
+	*This is the template for the header
+	*@package sunsettheme
+	*/
+
+ ?>
 <?php wp_footer() ?>
+</body>
+</html>
--- header.php
+++ header.php
@@ -1,1 +1,22 @@
-<?php wp_header() ?>
+<?php 
+	/*
+	*This is the template for the header
+	*@package sunsettheme
+	*/
+
+ ?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?> >
+<head>
+	<!-- theme to be consistent and validated by html validator --> 
+	<meta charset="<?php bloginfo('charset');?>">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!-- 	//necessary fo rhtml validation -->
+	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<?php if(is_singular() && pings_open(get_queried_object())); ?>
+		<link rel="pingback"  href="<?php bloginfo( 'pingback_url' ); ?>">
+	<?php endif; ?>
+	<?php wp_head(); ?>
+	<!-- <title></title> -->
+</head>
+<body <?php body_class(); ?>>
